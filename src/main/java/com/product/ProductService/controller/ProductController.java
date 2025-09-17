19aee5f738699388e1743dddccc635de6752b71c
boyee-zhang <b.zhang3@student.vu.nl>
2025-09-17 20:43:25 +0800

    complete swagger and readme

--- src/main/java/com/product/ProductService/controller/ProductController.java
+++ src/main/java/com/product/ProductService/controller/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/products")
+    @RequestMapping("/api/products")
 public class ProductController {
     private final ProductService productService;
 
